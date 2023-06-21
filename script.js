e3ba745a3c04a9516ae8db6fd5b119b8a95c1457
Brian Shaginaw <bshaginaw120@gmail.com>
2023-06-20 20:02:07 -0400

    Begin details modal

--- script.js
+++ script.js
@@ -5,12 +5,48 @@ const defaults = {
   name: "???",
 };
 
+window.addEventListener("click", (evt) => {
+  const entry = evt.target.closest(".mon-entry");
+  if (!entry) { return }
+  const mon = mons[entry.dataset.index];
+  if (!mon.sprite) { return }
+  let template = document.getElementById('details-template').innerHTML;
+  let compiled_template = Handlebars.compile(template);
+  let rendered = compiled_template({...defaults, ...mon});
+  document.getElementById('modal-inner').innerHTML = rendered;
+  openModal();
+});
+
 window.onload = function() {
   let template = document.getElementById('mon-entry').innerHTML;
   let compiled_template = Handlebars.compile(template);
   let rendered = "";
-  for (let mon of mons) {
-    rendered += compiled_template({...defaults, ...mon});
+  for (let [index, mon] of mons.entries()) {
+    rendered += compiled_template({index, ...defaults, ...mon});
   }
   document.getElementById('mon-list').innerHTML = rendered;
 };
+
+const closeModal = () => {
+  const modal = document.getElementById("modal");
+  modal.classList.add("closed");
+  window.setTimeout(() => { modal.style.visibility = "hidden"; }, 250);
+}
+
+const openModal = () => {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("closed");
+  modal.style.visibility = "visible";
+}
+
+document.getElementById("modal").addEventListener("click", event => {
+  if (!document.getElementById("modal-inner").contains(event.target)) {
+    closeModal();
+  }
+});
+
+document.addEventListener("keyup", event => {
+  if(event.key === "Escape") {
+    closeModal();
+  }
+});
